4c3dd1b886cd92bdd31092b59c6931f41e899c47
Narapureddy Gangadhar Reddy <gangadhar.5613@gmail.com>
2021-01-29 16:29:09 +0530

    Complete block-5

---
+++ block-BRaabs/code/working-on-forms/src/Components/Header.js
@@ -0,0 +1,19 @@
+
+
+
+function Header(){
+    return(
+        <header className='flex flex-col items-center justify-center bg-gray-800 text-white py-5'>
+            <div className='flex flex-row items-center my-2'>
+                <i class="fas fa-book mx-2 text-2xl"></i>
+                <h2 className='text-4xl'>Learning about forms</h2>
+            </div>
+            <div>
+                <h3 className='text-1xl'>Controlled Vs Uncontrolled Components</h3>
+            </div>
+        </header>
+    )
+}
+
+
+export default Header;
